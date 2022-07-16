1fc10ea3c5ddd55504e89047d551b17eeeb0e8bf
Fernando-Barajas-Gomez <fernando.bgomez@alumnos.udg.mx>
2022-07-16 16:48:00 -0500

    estilo del login

--- index.php
+++ index.php
@@ -8,9 +8,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/login/login.css">
     <title>Punto de venta</title>
 </head>
 <body>
-    
+    <div class="desenfoque centrar">
+        <form action="">
+            <h2>Login</h2>
+            <div class="campo">
+                <label for="usuario">Usuario:</label>
+                <input type="text" name="usuario" required>
+            </div>
+            <div class="campo">
+                <label for="password">Contraseña:</label>
+                <input type="text" name="password" required>
+            </div>
+            <div class="campo centrar">
+                <input type="submit" value="Ingresar">
+            </div>  
+        </form>
+    </div>
 </body>
 </html>
