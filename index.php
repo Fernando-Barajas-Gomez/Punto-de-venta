799721e01bff2c5fc96c267c62c54c4e43f9b204
Fernando-Barajas-Gomez <fernando.bgomez@alumnos.udg.mx>
2022-07-16 16:05:44 -0500

    fondo login

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-    echo "fiuuuum franchescoh vergoliiiiii";
+    //https://www.doabledanny.com/Deploy-PHP-And-MySQL-to-Heroku
+    //mysql://be22cb1bbe0616:dfb9dc9e@us-cdbr-east-06.cleardb.net/heroku_25a5b06ae795ac4?reconnect=true
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Punto de venta</title>
+</head>
+<body>
+    
+</body>
+</html>
