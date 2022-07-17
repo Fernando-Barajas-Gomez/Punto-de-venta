770f07b3d1cc92e907f5281f14104fadcb6ba33d
Fernando-Barajas-Gomez <fernando.bgomez@alumnos.udg.mx>
2022-07-16 19:07:06 -0500

    Archivo .htaccess para el control de acceso

--- views/consultaVentas.php
+++ views/consultaVentas.php
@@ -1,2 +1,7 @@
 <?php
+    session_start();
+    //si no se ha creado una sesion
+    if(!isset($_SESSION['usuario']) && !isset($_SESSION['id'])){
+        header('location: ../index.php'); //se le redirige al login
+    }
 ?>
